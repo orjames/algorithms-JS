fd3032c42e975d3bdee017eb63271174582e2bbd
orjames <orjames@berkeley.edu>
2019-06-25 14:17:28 -0700

    closure solution

--- bigO.js
+++ bigO.js
@@ -38,7 +38,9 @@ priceRange(hotels);
 // MEMOIZATION
 // Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
 // a simple multiplication fn
-const times10 = () => {};
+const times10 = n => {
+  return n * 10;
+};
 
 console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
 console.log('times10 returns:', times10(9));
@@ -49,8 +51,43 @@ console.log('times10 returns:', times10(9));
 
 const cache = {};
 
-const memoTimes10 = n => {};
+const memoTimes10 = n => {
+  if (n in cache) {
+    console.log('fetching from cache', n);
+    return cache;
+  } else {
+    console.log('calculating result');
+    let result = times10(n);
+    cache[n] = result;
+    return result;
+  }
+};
 
-console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
-console.log('Task 2 calculated value:', memoTimes10(9)); // calculated
-console.log('Task 2 cached value:', memoTimes10(9)); // cached
+const times10 = n => n * 10;
+
+// Task 3: Clean up your global scope by moving your cache inside your function.
+// protip: Use a closure to return a function that you can call later.
+
+const memoizedClosureTimes10 = () => {
+  let cache = {};
+  return n => {
+    if (n in cache) {
+      console.log('fetching from cache', n);
+      return cache;
+    } else {
+      console.log('calculating result');
+      let result = times10(n);
+      cache[n] = result;
+      return result;
+    }
+  };
+};
+
+const memoClosureTimes10 = memoizedClosureTimes10();
+console.log('~~~~~~~~~~~~~~TASK 3~~~~~~~~~~~~~~');
+try {
+  console.log('Task 3 calculated value:', memoClosureTimes10(9)); // calculated
+  console.log('Task 3 cached value:', memoClosureTimes10(9)); // cached
+} catch (e) {
+  console.error('Task 3:', e);
+}
